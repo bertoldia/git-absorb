9aae70f083ac58d03691d3dfa02cdc4e31528fbb
Axel von Bertoldi <bertoldia@gmail.com>
2017-01-13 15:11:27 -0700

    Initial rust version

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+extern crate git2;
+
+use std::env;
+use std::process::Command;
+use git2::Repository;
+use git2::string_array::StringArray;
+
+fn print_remotes(remotes: StringArray) {
+    for remote in remotes.iter() {
+        match remote {
+            Some(rem) => println!("{}", rem),
+            None => println!("Shitbox"),
+        }
+    }
+}
+
+fn main() {
+    let target_commit = env::args().nth(1).expect("Missing target commit.");
+
+    let repo = match Repository::open(".") {
+        Ok(repo) => repo,
+        Err(e) => panic!("failed to open: {}", e),
+    };
+
+    println!("{:?}", repo.state());
+
+    match repo.remotes() {
+        Ok(rem) => print_remotes(rem),
+        Err(e) => panic!("Oh shit! {}", e),
+    }
+
+    let output = Command::new("git")
+        .arg("rev-parse")
+        .arg(target_commit)
+        .output()
+        .expect("failed to execute process");
+
+    println!("status: {}", output.status);
+    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+}
