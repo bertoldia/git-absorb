33767c5e37a90ba1cda628f214a36cad5cbc6264
Axel von Bertoldi <bertoldia@gmail.com>
2017-01-15 21:17:38 -0700

    Add revparse via libgit

--- src/main.rs
+++ src/main.rs
@@ -27,15 +27,20 @@ fn main() {
     match repo.remotes() {
         Ok(rem) => print_remotes(rem),
         Err(e) => panic!("Oh shit! {}", e),
-    }
+    };
+
+    match repo.revparse_single(&target_commit) {
+        Ok(spec) => println!("{:?}", &spec.id()),
+        Err(e) => panic!("Invalid revspec {}: {}!", target_commit, e),
+    };
 
     let output = Command::new("git")
         .arg("rev-parse")
-        .arg(target_commit)
+        .arg(&target_commit)
         .output()
         .expect("failed to execute process");
 
-    println!("status: {}", output.status);
+    println!("status: {}", &output.status);
     println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
     println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
 }
