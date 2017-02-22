8f40432318dac78b745779a7dd66d4ccbe36fba4
Axel von Bertoldi <bertoldia@gmail.com>
2017-02-22 16:34:02 -0700

    Add support for tab completion of candidate commits

--- git-absorb.go
+++ git-absorb.go
@@ -7,14 +7,23 @@ import (
 )
 
 type Args struct {
-	Dryrun bool `short:"d" long:"dry-run" description:"Show the commit(s) the outstanding changes could successfully be absorbed into."`
-	Squash bool `short:"s" long:"squash" description:"squash instead of fixup the changes into the relevant commit. NOT IMPLEMENTED"`
-	Target struct {
+	Force           bool `short:"f" long:"force" description:"Perform the absorb operation instead of just showing the commit(s) the outstanding changes could successfully be absorbed into."`
+	Squash          bool `short:"s" long:"squash" description:"Squash instead of fixup the changes into the relevant commit. NOT IMPLEMENTED"`
+	PrintCandidates bool `short:"p" long:"print-candidates" description:"Print the sha1 of all candidate commits (i.e. all commits in the branch) in a human readable format."`
+	Machine         bool `short:"m" long:"machine-readable" description:"Print the candidate commits in a machine-parsable format."`
+	Target          struct {
 		Commit string `positional-arg-name:"target-commit"`
 	} `positional-args:"yes"`
 }
 
 var args Args
+
+func print_candidates(machine bool) {
+	if machine {
+		exit(0, "%s", strings.Join(commits_in_branch(), " "))
+	}
+	exit(0, "%s", strings.Join(human_commits(commits_in_branch()), "\n"))
+}
 
 func parse_args() *Args {
 	if _, err := flags.Parse(&args); err != nil {
@@ -37,6 +46,12 @@ func parse_args() *Args {
 func main() {
 	args := parse_args()
 
+	wd_is_git_repo()
+
+	if args.PrintCandidates {
+		print_candidates(args.Machine)
+	}
+
 	files, _ := files_to_absorb()
 	if len(files) == 0 {
 		exit(0, "Nothing to do...")
@@ -53,7 +68,7 @@ func main() {
 		target_commit = candidate_commits[0]
 	}
 
-	if !args.Dryrun {
+	if args.Force {
 		do_absorb(target_commit, args)
 	}
 	exit(0, "Absorbed changes into commit '%s'", human_commit(target_commit))
--- utils.go
+++ utils.go
@@ -35,6 +35,13 @@ func exec_cmd(cmd string, args ...string) []string {
 	return _parse_cmd_exec_result(res)
 }
 
+func wd_is_git_repo() {
+	res, err := exec.Command("git", "rev-parse", "--show-toplevel").CombinedOutput()
+	if err != nil {
+		exit(128, string(res))
+	}
+}
+
 func _parse_cmd_exec_result(res []byte) []string {
 	if len(res) == 0 {
 		return make([]string, 0)
@@ -129,7 +136,8 @@ func stash_pop() {
 // Expand a ref (in the form of a shortened sha1 or other valid ref spec like
 // HEAD) to the full sha1. Also useful for verifying the specified ref is valid.
 func expand_ref(sha1 string) (string, error) {
-	res, err := exec.Command("git", "rev-parse", "--verify", sha1).Output()
+	res, err := exec.Command("git", "rev-parse", "--verify",
+		strings.TrimRight(sha1, " ")).Output()
 	if err != nil {
 		return "", errors.New("'" + sha1 + "' is not a valid sha1.")
 	}
